45cbba25b50488f9b50e8014e9ea671d28b9bed4
linjimmy168 <linjimmy168@yahoo.com.tw>
2015-11-30 19:53:57 +0800

    first project update

---
+++ BookStore/Login.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace BookStore
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_login = new System.Windows.Forms.Label();
+            this.label_password = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button_enter = new System.Windows.Forms.Button();
+            this.button_cancel = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label_login
+            // 
+            this.label_login.AutoSize = true;
+            this.label_login.Location = new System.Drawing.Point(175, 101);
+            this.label_login.Name = "label_login";
+            this.label_login.Size = new System.Drawing.Size(56, 12);
+            this.label_login.TabIndex = 0;
+            this.label_login.Text = "登入帳號:";
+            // 
+            // label_password
+            // 
+            this.label_password.AutoSize = true;
+            this.label_password.Location = new System.Drawing.Point(188, 142);
+            this.label_password.Name = "label_password";
+            this.label_password.Size = new System.Drawing.Size(32, 12);
+            this.label_password.TabIndex = 1;
+            this.label_password.Text = "密碼:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(261, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(261, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button_enter
+            // 
+            this.button_enter.Location = new System.Drawing.Point(177, 209);
+            this.button_enter.Name = "button_enter";
+            this.button_enter.Size = new System.Drawing.Size(75, 23);
+            this.button_enter.TabIndex = 4;
+            this.button_enter.Text = "確認";
+            this.button_enter.UseVisualStyleBackColor = true;
+            this.button_enter.Click += new System.EventHandler(this.button_enter_Click);
+            // 
+            // button_cancel
+            // 
+            this.button_cancel.Location = new System.Drawing.Point(286, 209);
+            this.button_cancel.Name = "button_cancel";
+            this.button_cancel.Size = new System.Drawing.Size(75, 23);
+            this.button_cancel.TabIndex = 5;
+            this.button_cancel.Text = "取消";
+            this.button_cancel.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(243, 253);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(53, 12);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "忘記密碼";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 438);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button_cancel);
+            this.Controls.Add(this.button_enter);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_password);
+            this.Controls.Add(this.label_login);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_login;
+        private System.Windows.Forms.Label label_password;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button_enter;
+        private System.Windows.Forms.Button button_cancel;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
---
+++ BookStore/MainForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace BookStore
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.label_Dept = new System.Windows.Forms.Label();
+            this.label_Emp = new System.Windows.Forms.Label();
+            this.comboBox_Dept = new System.Windows.Forms.ComboBox();
+            this.comboBox_Emp = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(689, 491);
+            this.splitContainer1.SplitterDistance = 190;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.comboBox_Emp);
+            this.splitContainer2.Panel2.Controls.Add(this.comboBox_Dept);
+            this.splitContainer2.Panel2.Controls.Add(this.label_Emp);
+            this.splitContainer2.Panel2.Controls.Add(this.label_Dept);
+            this.splitContainer2.Size = new System.Drawing.Size(190, 491);
+            this.splitContainer2.SplitterDistance = 112;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // label_Dept
+            // 
+            this.label_Dept.AutoSize = true;
+            this.label_Dept.Location = new System.Drawing.Point(34, 5);
+            this.label_Dept.Name = "label_Dept";
+            this.label_Dept.Size = new System.Drawing.Size(53, 12);
+            this.label_Dept.TabIndex = 0;
+            this.label_Dept.Text = "選擇部門";
+            // 
+            // label_Emp
+            // 
+            this.label_Emp.AutoSize = true;
+            this.label_Emp.Location = new System.Drawing.Point(34, 41);
+            this.label_Emp.Name = "label_Emp";
+            this.label_Emp.Size = new System.Drawing.Size(33, 12);
+            this.label_Emp.TabIndex = 1;
+            this.label_Emp.Text = "label2";
+            // 
+            // comboBox_Dept
+            // 
+            this.comboBox_Dept.FormattingEnabled = true;
+            this.comboBox_Dept.Location = new System.Drawing.Point(36, 20);
+            this.comboBox_Dept.Name = "comboBox_Dept";
+            this.comboBox_Dept.Size = new System.Drawing.Size(121, 20);
+            this.comboBox_Dept.TabIndex = 2;
+            // 
+            // comboBox_Emp
+            // 
+            this.comboBox_Emp.FormattingEnabled = true;
+            this.comboBox_Emp.Location = new System.Drawing.Point(36, 56);
+            this.comboBox_Emp.Name = "comboBox_Emp";
+            this.comboBox_Emp.Size = new System.Drawing.Size(121, 20);
+            this.comboBox_Emp.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 491);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ComboBox comboBox_Emp;
+        private System.Windows.Forms.ComboBox comboBox_Dept;
+        private System.Windows.Forms.Label label_Emp;
+        private System.Windows.Forms.Label label_Dept;
+    }
+}
+
